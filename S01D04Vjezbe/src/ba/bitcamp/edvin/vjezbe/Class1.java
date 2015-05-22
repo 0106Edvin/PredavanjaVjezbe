f27b84edf9e4418cffdf6195cd595901a455825e
0106Edvin <edvin.mulabdic@gmail.com>
2015-05-22 11:34:54 +0200

    PredavanjaVjezbe

---
+++ S01D04Vjezbe/src/ba/bitcamp/edvin/vjezbe/Class1.java
@@ -0,0 +1,18 @@
+package ba.bitcamp.edvin.vjezbe;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 6;
+		int num1 = 4;
+		boolean c = (num - num1) < 0;
+		
+		
+	
+		System.out.println("a je vece od b:" + c);
+				
+
+	}
+
+}
---
+++ Variable/src/Class1.java
@@ -0,0 +1,9 @@
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int binaryNum = 0xFACEB008;
+
+		System.out.println(binaryNum);
+	}
+}
